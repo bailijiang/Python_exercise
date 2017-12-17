1ee4ebcc80134d38725e90079e056d0e4f4e45ef
bailijiang <bailijiang@hotmail.com>
2017-12-17 17:04:41 +0800

    chap 6.3 ...

---
+++ favorite_language.py
@@ -0,0 +1,13 @@
+__author__ = 'Bryan'
+
+favorite_languages = {
+    'bob': 'Python',
+    'john': 'Java',
+    'leon': 'C',
+    'heero': 'Golang',
+    'bryan': 'C++'
+}
+
+print("bryan's favorite language is: " +
+      str(favorite_languages['bryan']).title() +
+      ".")
--- toppings.py
+++ toppings.py
@@ -1,6 +1,31 @@
 __author__ = 'Bryan'
 
-requesting_topping = 'mushrooms'
+# requesting_topping = 'mushrooms'
+#
+# if requesting_topping != 'anchovies':
+#     print("Hold the anchovies!")
 
-if requesting_topping != 'anchovies':
-    print("Hold the anchovies!")
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+# requested_toppings = []
+#
+# if requested_toppings:
+#     for requested_topping in requested_toppings:
+#         if requested_topping == 'green peppers':
+#             print("Sorry, we are out of green peppers now!")
+#         else:
+#             print("Adding " + requested_topping)
+# else:
+#     print("Are you sure you want a plain pizza?")
+
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Adding " + requested_topping + ".")
+    else:
+        print("Sorry, we don't have " + requested_topping + ".")
+print("\nFinishing making your pizza!")
+
