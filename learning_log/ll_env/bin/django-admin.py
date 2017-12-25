62f71352fdd2ff609c7c27ae0307552d2cfce6af
bailijiang <bailijiang@hotmail.com>
2017-12-25 12:46:48 +0800

    chapter 18.1 django2.0 ...

---
+++ learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Bryan/Documents/Code/Python_exercise/learning_log/ll_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
