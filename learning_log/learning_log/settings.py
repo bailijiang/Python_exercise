b944164d1d9617da187f7ccf72b61b1d5878d767
bailijiang <bailijiang@hotmail.com>
2017-12-30 07:37:24 +0800

    chapter 20 Django finish ...

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5didhkh#he3b^6%koj@vbi+0$c+ole6)$-&=%v)+yw103xi_*$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
